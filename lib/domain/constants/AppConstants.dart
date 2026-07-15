cd2a10e6d1b8b365f68a5b8f23e271af77b64b3f
ganeshlg <ganeshlg2108@gmail.com>
2026-07-15 13:29:41 +0530

    Resolved Horizontal Scroll Issue

--- lib/domain/constants/AppConstants.dart
+++ lib/domain/constants/AppConstants.dart
@@ -1,4 +1,5 @@
 class AppConstants {
   // static const baseUrl = 'http://localhost:8000/api';
-  static const baseUrl = 'https://learning-management-system-api-gateway-v1.onrender.com/api';
+  // static const baseUrl = 'https://learning-management-system-api-gateway-v1.onrender.com/api';
+  static const baseUrl = 'https://learning-management-system-api-gateway-v1-3nhc.onrender.com/api';
 }
